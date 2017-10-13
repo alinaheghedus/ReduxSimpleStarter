e6218b3aa9715c7e8b01858d6863b87d47af8a66
Alina Heghedus <alinaheghedus@yahoo.com>
2017-10-13 16:03:31 -0400

    create videos

--- src/index.js
+++ src/index.js
@@ -1,21 +1,49 @@
-import React from 'react';
+import React, { Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+//import VideoListItem from './components/video_list_item';
 
 const API_KEY = 'AIzaSyAPKx7ZLZHx2dWxJ2ypsV0ejvfkNQK_eSA';
 
 
-// create a new component that should produce some HTML
-const App = () => {
-	return (
-	<div>
-		<SearchBar />
-	</div>
-	);
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { 
+			videos: [],
+			selectedVideo: null
+		};
+
+		this.videoSearch('surfboards');
+	}
+
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({ 
+				videos: videos,
+				selectedVideo: videos[0] 
+			});
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+				<VideoDetail video={this.state.selectedVideo}/>
+				<VideoList 
+					onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+					videos={this.state.videos} />
+			</div>
+		);
+	}
 }
 
-// take this component and put it on the page (in the DOM)
+
 ReactDOM.render(<App />, document.querySelector('.container'));
 
